5910044d6a53e3d7bca747ebf1b76ad72ca06185
Carla Moema <carlamoema@gmail.com>
2024-01-23 16:11:33 -0300

    Update 1_Central_de_Risco.py

--- pages/1_Central_de_Risco.py
+++ pages/1_Central_de_Risco.py
@@ -317,7 +317,7 @@ with tab1: # Goiás
           ## Gráfico de Colunas contendo 5 maiores tomadores (ticket médio) e o desvio padrão por ocupação
           #def time_bar_city(df1):
           cols = ['ocupacao', 'carteira_ativa']
-          df_aux=df4.loc[:,cols ].groupby('ocupacao').agg({'carteira_ativa': ['mean', 'std']})
+          df_aux=df4.loc[:,cols ].groupby('ocupacao').agg({'carteira_ativa': ['mean', 'std']}).sort_values('mean', ascending=True)
           df_aux.columns = ['Media', 'Desvio_padrao']
           df_aux = df_aux.head(5).reset_index()
           fig = go.Figure()
