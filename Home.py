c8b2322e40fd2af8fe596c05a287d7760b5fb694
Carla Moema <104834770+carlamoema@users.noreply.github.com>
2024-01-23 13:42:25 -0300

    Substituicao do ícone da Home
    Substituição do ícone 'grafico' da Home por ícone 'money'.

--- Home.py
+++ Home.py
@@ -3,7 +3,7 @@ from PIL import Image
 
 st.set_page_config( 
                    page_title="Home",
-                   page_icon="🎲",  layout='wide')
+                   page_icon="💵",  layout='wide')
 
 st.title(' Informações de Crédito ')
 
