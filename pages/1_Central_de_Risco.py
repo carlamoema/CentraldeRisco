a38d00edc9fadaa0feb5c98ab8a327ab7ec9b090
Carla Moema <carlamoema@gmail.com>
2024-01-23 16:48:33 -0300

    Revert "Update 1_Central_de_Risco.py"
    This reverts commit 4819d9f268cd80b47c19ba24f47aa5a33405786b.

--- pages/1_Central_de_Risco.py
+++ pages/1_Central_de_Risco.py
@@ -319,10 +319,7 @@ with tab1: # Goiás
           cols = ['ocupacao', 'carteira_ativa']
           df_aux=df4.loc[:,cols ].groupby('ocupacao').agg({'carteira_ativa': ['mean', 'std']})
           df_aux.columns = ['Media', 'Desvio_padrao']
-<<<<<<< HEAD
           df_aux1=df_aux.loc[:,['ocupacao', 'Media', 'Desvio_padrao']].sort_values('Media', ascending=False)
-=======
->>>>>>> parent of d0f27fe (Update 1_Central_de_Risco.py)
           fig = go.Figure()
           fig.add_trace(go.Bar(name='Control', x=df_aux['ocupacao'], y=df_aux['Media'], error_y=dict(type='data', array=df_aux['Desvio_padrao'])))
           st.plotly_chart(fig, use_container_width=True)
